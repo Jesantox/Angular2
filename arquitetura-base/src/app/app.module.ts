c5ef7a60fb23c9e192692668e338f39e404f2db0
Jesantox <jean.costa@portoseguro.com.br>
2022-05-08 07:05:34 -0300

    ⭕️ Componente Inicial
    🟢 Chamada do componente
    🟢 Criação de variável
    🟢 Interpolação de variável

--- arquitetura-base/src/app/app.module.ts
+++ arquitetura-base/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteInicialComponent } from './componente-inicial/componente-inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteInicialComponent
   ],
   imports: [
     BrowserModule,
---
+++ arquitetura-base/src/app/componente-inicial/componente-inicial.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-inicial',
+  templateUrl: './componente-inicial.component.html',
+  styleUrls: ['./componente-inicial.component.scss']
+})
+export class ComponenteInicialComponent implements OnInit {
+
+  public title: string = 'Você conseguiu gerar o primeiro componente !!'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
