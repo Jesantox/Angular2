5b600c03b1ccff6d22407213b9ab2ce099f5f186
Jesantox <jean.costa@portoseguro.com.br>
2022-05-09 16:08:06 -0300

    ⭕️ Componente Title - Ciclo de vida ngOnChanges
    👉 Evento disparado sempre que um componente recebe um dado externo
    👉 Criando componente Title
    👉 Implementando componentes
    👉 Manipulando dados de entrada

--- arquitetura-base/src/app/app.module.ts
+++ arquitetura-base/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponenteInicialComponent } from './componente-inicial/componente-inicial.component';
+import { TextoComponent } from './texto/texto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponenteInicialComponent
+    ComponenteInicialComponent,
+    TextoComponent
   ],
   imports: [
     BrowserModule,
--- arquitetura-base/src/app/componente-inicial/componente-inicial.component.ts
+++ arquitetura-base/src/app/componente-inicial/componente-inicial.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-componente-inicial',
   templateUrl: './componente-inicial.component.html',
   styleUrls: ['./componente-inicial.component.scss']
 })
-export class ComponenteInicialComponent implements OnInit {
+export class ComponenteInicialComponent implements OnInit, OnChanges {
 
-  public title: string = 'Você conseguiu gerar o primeiro componente !!'
+  public title: string = 'texto padrão do componente-inicial';
+  @Input() public idade: string = '12'
 
   constructor() { }
 
@@ -15,6 +16,10 @@ export class ComponenteInicialComponent implements OnInit {
     setTimeout( () => {
       console.log('ngOnInit startado após 5 seg')
     }, 5000)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
   }
 
 }
